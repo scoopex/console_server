fbacabbd36d1c34ae043ea08b3760e27709f20d8
Marc Schöchlin <schoechlin@osb-alliance.com>
2023-12-28 18:47:45 +0100

    add

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,7 @@ use toml::Value;
 
 pub struct GlobalConfig {
     pub socket_base_path: String,
+    pub log_file: String,
     pub users_rw: Vec<String>,
 }
 
@@ -70,6 +71,11 @@ fn parse_global_config(toml_value: &Value) -> GlobalConfig {
           .and_then(Value::as_str)
           .unwrap_or("/tmp/example_socket")
           .to_string(),
+      log_file: global_section
+          .get("log_file")
+         .and_then(Value::as_str)
+         .unwrap_or("/console_server.log")
+         .to_string(),
       users_rw: global_section
           .get("users_rw")
           .and_then(Value::as_array)
@@ -81,43 +87,51 @@ fn parse_global_config(toml_value: &Value) -> GlobalConfig {
 }
 
 fn parse_serial_configs(toml_value: &Value) -> Vec<SerialConfig> {
-  toml_value["serial"]
-      .as_array()
-      .unwrap()
-      .iter()
-      .map(|serial_section| SerialConfig {
-          name: serial_section["name"].as_str().unwrap_or_default().to_string(),
-          users_rw: serial_section
-              .get("users_rw")
-              .and_then(Value::as_array)
-              .unwrap()
-              .iter()
-              .filter_map(|v| v.as_str().map(String::from))
-              .collect(),
-          serial_device_name: serial_section
-              .get("serial_device_name")
-              .and_then(Value::as_str)
-              .unwrap_or_default()
-              .to_string(),
-      })
-      .collect()
+    if let Some(serial_array) = toml_value.get("serial_console").and_then(Value::as_array) {
+        return serial_array
+            .iter()
+            .map(|serial_section| SerialConfig {
+                name: serial_section["name"].as_str().unwrap_or_default().to_string(),
+                users_rw: serial_section
+                    .get("users_rw")
+                    .and_then(Value::as_array)
+                    .unwrap()
+                    .iter()
+                    .filter_map(|v| v.as_str().map(String::from))
+                    .collect(),
+                serial_device_name: serial_section
+                    .get("serial_device_name")
+                    .and_then(Value::as_str)
+                    .unwrap_or_default()
+                    .to_string(),
+            })
+            .collect();
+    }
+
+    return Vec::new()
 }
 
 fn parse_dummy_configs(toml_value: &Value, socket_base_path: &String) -> Vec<DummyConfig> {
-  toml_value["dummy"]
-      .as_array()
-      .unwrap()
-      .iter()
-      .map(|dummy_section| DummyConfig {
-          name: dummy_section["name"].as_str().unwrap_or_default().to_string(),
-          socket_path: format!("{}_{}", socket_base_path, dummy_section["name"].as_str().unwrap_or_default().to_string()),
-          users_rw: dummy_section
-              .get("users_rw")
-              .and_then(Value::as_array)
-              .unwrap()
-              .iter()
-              .filter_map(|v| v.as_str().map(String::from))
-              .collect(),
-      })
-      .collect()
+    if let Some(dummy_array) = toml_value.get("dummy_console").and_then(Value::as_array) {
+        return dummy_array
+            .iter()
+            .map(|dummy_section| DummyConfig {
+                name: dummy_section["name"].as_str().unwrap_or_default().to_string(),
+                socket_path: format!(
+                    "{}_{}",
+                    socket_base_path,
+                    dummy_section["name"].as_str().unwrap_or_default().to_string()
+                ),
+                users_rw: dummy_section
+                    .get("users_rw")
+                    .and_then(Value::as_array)
+                    .unwrap()
+                    .iter()
+                    .filter_map(|v| v.as_str().map(String::from))
+                    .collect(),
+            })
+            .collect();
+    }
+
+    return Vec::new();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 
-
 use std::thread;
 use console_server::console::{Console, create_listener};
 use console_server::config::{ServerConfig, load_config};
 
 fn main() {
+    env_logger::Builder::new()
+    .filter_level(log::LevelFilter::Info)
+    .format_timestamp_secs()
+    .init();
+
     unsafe { libc::umask(0o077) };
     let cfg: ServerConfig = load_config("example.toml");
 
     let mut console_list: Vec<Console> = Vec::new();
     
-    println!("Dummy consoles:");
-    println!("*****************************************************");
     for dummy_config in cfg.dummy {
-        println!("Name: {}", dummy_config.name);
+        log::info!("Dummy Console {} for ", dummy_config.name);
         println!("Users_RW: {:?}", dummy_config.users_rw);
         println!("Socket Path: {:?}", dummy_config.socket_path);
         println!("\nTry it, with:\nsocat - UNIX-CONNECT:{}", dummy_config.socket_path);
