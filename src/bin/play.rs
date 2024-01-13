be5ff727d686e216758164e8ef2b503173657de3
Marc Schöchlin <ms-github@256bit.org>
2024-01-13 14:31:55 +0100

    broadcast example

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -2,34 +2,36 @@
 use std::thread;
 use std::time::Duration;
 use bus::Bus;
+use std::sync::{Arc,Mutex};
+
 fn main() {
 
-    let mut bus = Bus::new(5);
-    let mut rx1 = bus.add_rx();
-    let mut rx2 = bus.add_rx();
+    let bus = Arc::new(Mutex::new(Bus::new(10)));
 
-    thread::spawn(move || {
-        thread::sleep(Duration::from_secs(2));
-        let mut count = 0;
-        loop {
-            println!("recv thread 1: {}, {} values received", rx1.recv().unwrap(), count);
-            count += 1;
-        }
-    });
-    thread::spawn(move || {
-        let mut count = 0;
-        loop {
-            println!("recv thread 2: {}, {} values received", rx2.recv().unwrap(), count);
-            count += 1;
-        }
-    });
-
-    let mut count = 0;
-    loop{
-        bus.broadcast(count);
-        count += 1;
-        thread::sleep(Duration::from_secs(1));
+    for thread_nr in 1..10 {
+        println!("Starting thread {} now", thread_nr);
+        let also_bus = bus.clone();
+        thread::spawn(move || {
+            let mut rx1 = also_bus.lock().unwrap().add_rx();
+            thread::sleep(Duration::from_secs(2));
+            let mut count = 0;
+            loop {
+                println!("recv thread {:?}: {}, {} values received", thread::current().id(), rx1.recv().unwrap(), count);
+                count += 1;
+            }
+        });
     }
+
+   thread::spawn(move || {
+       let mut count = 0;
+       let mut real_bus = bus.lock().unwrap();
+       loop {
+           real_bus.broadcast(count.to_string());
+           count += 1;
+           thread::sleep(Duration::from_secs(1));
+       }
+   });
+    thread::park();
 }
 
 
